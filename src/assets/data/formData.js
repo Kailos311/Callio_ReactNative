68b57494701d8d43b9de0e715703525673cce89e
Kailos311 <joseph.devstar@gmail.com>
2023-07-13 13:12:48 -0700

    assets

---
+++ src/assets/data/formData.js
@@ -0,0 +1,92 @@
+export const signupFormData = [
+  {
+    id: 1,
+    title: 'Name',
+    name: 'name',
+  },
+  {
+    id: 2,
+    title: 'Email',
+    name: 'email',
+  },
+  {
+    id: 3,
+    title: 'Age',
+    name: 'age',
+  },
+  {
+    id: 4,
+    title: 'Gender',
+    name: 'gender',
+  },
+  {
+    id: 5,
+    title: 'Phone',
+    name: 'phone',
+  },
+  {
+    id: 6,
+    title: 'User Type',
+    name: 'userType',
+  },
+  {
+    id: 7,
+    title: 'Password',
+    name: 'password',
+  },
+];
+
+export const loginFormData = [
+  {
+    id: 1,
+    title: 'Email',
+    name: 'email',
+    iconName: 'email',
+  },
+  {
+    id: 2,
+    title: 'Password',
+    name: 'password',
+    iconName: 'lock',
+  },
+];
+
+export const changePassword = [
+  {
+    id: 1,
+    title: 'New Password',
+    name: 'password',
+  },
+  {
+    id: 2,
+    title: 'Re-Enter Password',
+    name: 'password2',
+  },
+];
+
+export const editProfile = [
+  {
+    id: 1,
+    title: 'Name',
+    name: 'name',
+    required: false,
+  },
+  {
+    id: 2,
+    title: 'Email',
+    name: 'email',
+    required: false,
+  },
+  {
+    id: 3,
+    title: 'Phone',
+    name: 'phone',
+    required: false,
+  },
+  {
+    id: 4,
+    title: 'Gender',
+    name: 'gender',
+    required: false,
+  },
+];
